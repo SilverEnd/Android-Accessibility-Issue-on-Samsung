fe7ba9ea56a65c4e6943dfd4a7ff5a7ab1758d84
nyzo1 <yzuo@woolworths.com.au>
2019-07-29 15:17:57 +1000

    Clean up master branch

--- app/src/main/java/com/erikzuo/androidplayground/MainActivity.kt
+++ app/src/main/java/com/erikzuo/androidplayground/MainActivity.kt
@@ -13,59 +13,9 @@ import com.erikzuo.androidplayground.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var activityBinding: ActivityMainBinding
-    private val adapter = MyAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         activityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        activityBinding.list.apply {
-            adapter = this@MainActivity.adapter
-            layoutManager = LinearLayoutManager(this@MainActivity)
-        }
-
-        adapter.setData()
-    }
-}
-
-class MyAdapter : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
-
-    private val myDataset: MutableList<String> = mutableListOf()
-
-    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): MyAdapter.MyViewHolder {
-        val textView = LayoutInflater.from(parent.context).inflate(
-            R.layout.my_text_view, parent, false
-        ) as TextView
-
-        return MyViewHolder(textView)
-    }
-
-    override fun onBindViewHolder(
-        holder: MyViewHolder,
-        position: Int
-    ) {
-        holder.textView.text = myDataset[position]
-    }
-
-    override fun getItemCount() = myDataset.size
-
-    fun setData() {
-        val newList = mutableListOf<String>()
-        for (i in 0..100) {
-            newList.add("string $i")
-        }
-
-        myDataset.apply {
-            clear()
-            addAll(newList)
-        }
-
-        notifyDataSetChanged()
     }
 }
