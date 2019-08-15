e21459c8bb65448f97f6ec584884deea28753e66
nyzo1 <yzuo@woolworths.com.au>
2019-08-15 13:07:06 +1000

    Progress bar in recycler view item re-bind issue

--- app/src/main/java/com/erikzuo/androidplayground/MainActivity.kt
+++ app/src/main/java/com/erikzuo/androidplayground/MainActivity.kt
@@ -1,21 +1,71 @@
 package com.erikzuo.androidplayground
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.epoxy.TypedEpoxyController
 import com.erikzuo.androidplayground.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), StartLoadingHandler {
 
     private lateinit var activityBinding: ActivityMainBinding
+    private val myItems = MutableList(50) { index -> MyItem(index, false) }
+    private lateinit var controller: MyController
 
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        controller = MyController(this)
+        activityBinding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            setController(controller)
+        }
+
+        activityBinding.resetButton.setOnClickListener {
+            myItems.replaceAll {
+                if (it.value) {
+                    it.copy(value = false)
+                } else {
+                    it.copy()
+                }
+            }
+            controller.setData(myItems)
+        }
+
+        controller.setData(myItems)
+    }
+
+    override fun onStartLoadingClick(id: Int) {
+        myItems[id] = MyItem(id, true)
+        controller.setData(myItems)
+    }
+}
+
+interface StartLoadingHandler {
+    fun onStartLoadingClick(id: Int)
+}
+
+data class MyItem(
+    val id: Int,
+    val value: Boolean
+)
+
+class MyController(
+    private val handler: StartLoadingHandler
+) : TypedEpoxyController<List<MyItem>>() {
+
+    override fun buildModels(data: List<MyItem>) {
+        data.forEach {
+            itemRow {
+                id(it.id)
+                item(it)
+                handler(this@MyController.handler)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/erikzuo/androidplayground/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2019 Woolworths. All rights reserved.
+ */
+
+@EpoxyDataBindingLayouts({
+        R.layout.item_row
+})
+package com.erikzuo.androidplayground;
+
+import com.airbnb.epoxy.EpoxyDataBindingLayouts;
